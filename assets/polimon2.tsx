09a1bd6690554094bcec2f09afa902ff2f90ea0e
patitow <msoliver2002@gmail.com>
2022-09-16 14:55:18 -0300

    Mapa inicial e sprites temporarios

---
+++ assets/polimon2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="polimon2" tilewidth="16" tileheight="16" tilecount="35" columns="7">
+ <image source="notfinal/Sprout Lands - Sprites - Basic pack/Tilesets/Wooden House.png" width="112" height="80"/>
+</tileset>
